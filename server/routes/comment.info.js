c05042eb16b1d8b3fef137327f0fb6c1ebff7fe7
RichardAPena <richardalexpena@hotmail.com>
2022-04-27 12:46:54 -0400

    comment and post routes

---
+++ server/routes/comment.info.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+let Comment = require("../model/comment.model");
+
+const auth = require("./auth/middleware/auth");
+
+// get comment by id
+
+// get comment by user id
+
+// get comment by post id
+
+// get all comments
+
+// get comment by user and post id
+
+// add comment
+
+// delete comment
+
+module.exports = router;
